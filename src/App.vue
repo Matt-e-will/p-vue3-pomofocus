6f05fbc6477d88480274fb510e4f82a232fce033
William Wenling Veiga <140337500+william-wv@users.noreply.github.com>
2025-02-22 20:49:23 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Timer from "./components/timer.vue"
+import Header from "./components/Header.vue"
+</script>
+
+
+<template>
+  <body>
+    
+    <Header/>
+    <Timer/>
+    
+  </body>
+
+</template>
+
+
